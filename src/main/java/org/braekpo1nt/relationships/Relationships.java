489738df92470c103982109b0a2a91b037ff27c7
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-11-02 10:26:55 -0500

    added dependencies

---
+++ src/main/java/org/braekpo1nt/relationships/Relationships.java
@@ -0,0 +1,17 @@
+package org.braekpo1nt.relationships;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Relationships extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
