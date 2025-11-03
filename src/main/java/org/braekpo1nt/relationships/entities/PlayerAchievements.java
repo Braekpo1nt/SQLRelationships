7c9d5780dd3eceec0fe2979ad534f378cd4dae5c
Braekpo1nt <braekpo1ntofficial@gmail.com>
2025-11-03 10:02:53 -0500

    created Achievement join table

---
+++ src/main/java/org/braekpo1nt/relationships/entities/PlayerAchievements.java
@@ -0,0 +1,56 @@
+package org.braekpo1nt.relationships.entities;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.Date;
+
+/**
+ * The join table, represents the relationship between a single player and a single achievement
+ */
+@DatabaseTable(tableName = "player_achievements")
+public class PlayerAchievements {
+    
+    @DatabaseField(generatedId = true)
+    private int id;
+    
+    @DatabaseField(foreign = true, foreignAutoRefresh = true, columnName = "player_id")
+    private GuildPlayer guildPlayer;
+    
+    @DatabaseField(foreign = true, foreignAutoRefresh = true, columnName = "achievement_id")
+    private Achievement achievement;
+    
+    @DatabaseField(canBeNull = false)
+    private Date dateAchieved;
+    
+    public PlayerAchievements() {
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public GuildPlayer getGuildPlayer() {
+        return guildPlayer;
+    }
+    
+    public void setGuildPlayer(GuildPlayer guildPlayer) {
+        this.guildPlayer = guildPlayer;
+    }
+    
+    public Achievement getAchievement() {
+        return achievement;
+    }
+    
+    public void setAchievement(Achievement achievement) {
+        this.achievement = achievement;
+    }
+    
+    public Date getDateAchieved() {
+        return dateAchieved;
+    }
+    
+    public void setDateAchieved(Date dateAchieved) {
+        this.dateAchieved = dateAchieved;
+    }
+}
